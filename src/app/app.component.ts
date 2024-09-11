92cf34d4ab29ed4e206c3be55c25cc76f695b804
Debdeepd03 <debdeep22102003@gmail.com>
2024-09-11 09:54:38 +0530

    do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo2';
+  message:string | undefined;
+  private s:StudentService;
+  constructor(ss:StudentService){
+    this.s=ss;
+    alert("I am Constructor!!")
+  }
+ngOnInit()
+{
+this.message=this.s.getTitle();
+alert("I am NgOnInit!!")
+}
 }
---
+++ src/app/student.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor() { }
+getTitle()
+{
+return "Dependency Injection in Angular  by Debdeep";
+}
+}
